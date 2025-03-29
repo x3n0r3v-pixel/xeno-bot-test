db7a2a3f66b8daa51c3f5533e751b28e30e50d8b
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-03-29 23:57:38 +0100

    Update gp-abbraccio.js

--- plugins/gp-abbraccio.js
+++ plugins/gp-abbraccio.js
@@ -1,15 +1,1 @@
-let handler = async (m, { conn, command, text }) => {
-    if (!text) throw `Tagga chi desideri abbracciare`;
-    if (m.isReplied) return; // Evita risposte multiple
-    m.isReplied = true; // Segna il messaggio come già risposto
 
-    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
-    conn.reply(m.chat, `*stai abbracciando @${user.split('@')[0]}*`, m, { mentions: [user, m.sender] });
-    
-    conn.sendMessage(m.chat, { react: { text: '🫂', key: m.key } });
-}
-
-handler.customPrefix = /^(.abbraccia)$/i
-handler.admin = true;
-handler.command = new RegExp;
-export default handler;
