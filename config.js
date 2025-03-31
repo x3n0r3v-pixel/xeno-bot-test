264bb51c5a1253fa835341010c533ba1ce1bed0c
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-03-31 09:39:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -90,4 +90,4 @@ let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
   console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)k
+  import(`${file}?update=${Date.now()}`)
