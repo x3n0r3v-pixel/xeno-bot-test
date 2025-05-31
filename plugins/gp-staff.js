6f4c405ff8ed288ae4e2e4db821f255152d4a58b
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-06-01 01:47:25 +0200

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -18,14 +18,14 @@ END:VCARD`.replace(/\n/g, '\r\n'); // Importante per la compatibilità Android
                 { vcard: createVCard('Creatore', '393515533859', 'Creatore') },
                 { vcard: createVCard('Finanziatore', '393803482529', 'Finanziatore') },
                 { vcard: createVCard('Social Media Manager', '639076516872', 'Social Media') },
-                
+                 { vcard: createVCard('Promoter', '393534243103', 'Promoter') }
                 // Developer
                 { vcard: createVCard('Developer 1', '393935731102', 'Developer') },
                 { vcard: createVCard('Developer 2', '447365589428', 'Developer') },
                 { vcard: createVCard('Developer 3', '393291958600', 'Developer') },
                 { vcard: createVCard('Developer 4', '393518419909', 'Developer') },
                 { vcard: createVCard('Developer 5', '639649477630', 'Developer') },
-                { vcard: createVCard('Developer 6', '393534243103', 'Promoter') } // Nuovo contatto aggiunto
+             // Nuovo contatto aggiunto
             ] 
         } 
     }, { quoted: m });
