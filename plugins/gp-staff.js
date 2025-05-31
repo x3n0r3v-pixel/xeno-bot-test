5abd9df5e9c4a6e7b82edaa1235354ec5ede8d01
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-06-01 01:50:49 +0200

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -1,44 +1,39 @@
 let handler = async (m, { conn }) => {
-    // Funzione per generare vCard correttamente formattate
-    const createVCard = (name, number, label) => {
+    // Funzione per generare vCard perfettamente formattate
+    const createVCard = (name, number, role) => {
         return `BEGIN:VCARD
 VERSION:3.0
 FN:${name}
 ORG:ChatUnity;
-TEL;type=CELL;type=VOICE;waid=${number}:${number}
-X-ABLabel:${label}
-END:VCARD`.replace(/\n/g, '\r\n'); // Importante per la compatibilità Android
+TEL;type=CELL;type=VOICE;waid=${number}:+${number}
+X-ABLabel:${role}
+END:VCARD`.replace(/\n/g, '\r\n');
     };
 
     await conn.sendMessage(m.chat, { 
         contacts: { 
-            displayName: 'ChatUnity Staff', 
+            displayName: '👥 ChatUnity Staff', 
             contacts: [
-                // Contatti originali
-                { vcard: createVCard('Creatore', '393515533859', 'Creatore') },
-                { vcard: createVCard('Finanziatore', '393803482529', 'Finanziatore') },
-                { vcard: createVCard('Social Media Manager', '639076516872', 'Social Media') },
+                // Team Principale
+                { vcard: createVCard('Creatore', '393515533859', 'Founder') },
+                { vcard: createVCard('Finanziatore', '393803482529', 'Financer') },
+                { vcard: createVCard('Social Manager', '639076516872', 'Social Media') },
                 { vcard: createVCard('Promoter', '393534243103', 'Promoter') },
                 
-                // Developer
+                // Team Sviluppo
                 { vcard: createVCard('Developer 1', '393935731102', 'Developer') },
                 { vcard: createVCard('Developer 2', '447365589428', 'Developer') },
                 { vcard: createVCard('Developer 3', '393291958600', 'Developer') },
                 { vcard: createVCard('Developer 4', '393518419909', 'Developer') },
-                { vcard: createVCard('Developer 5', '639649477630', 'Developer') }
-            ] 
-        } 
+                { vcard: createVCard('Developer 5', '639649477630', 'Developer') },
+                { vcard: createVCard('Developer 6', '393534243103', 'Developer') } // Developer aggiuntivo
+            ]
+        }
     }, { quoted: m });
 };
 
+// Comandi e metadati
 handler.help = ['staff'];
 handler.tags = ['info'];
-handler.command = ['staff'];
-
-export default handler;
-
-handler.help = ['staff'];
-handler.tags = ['info'];
-handler.command = ['staff'];
-
+handler.command = ['staff', 'team'];
 export default handler;
