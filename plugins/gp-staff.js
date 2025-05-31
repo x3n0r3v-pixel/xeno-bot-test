24bc93a2f6706be0a9e33b62738d824672789ce8
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-06-01 01:30:45 +0200

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -1,29 +1,40 @@
 let handler = async (m, { conn }) => {
-    let vcardCreator = `BEGIN:VCARD\nVERSION:4.0\nN:;Creatore;;\nFN:Creatore\nORG:ChatUnity\nTITLE:\nitem1.TEL;waid=393515533859:393515533859\nitem1.X-ABLabel:Creatore\nEND:VCARD`;
-    let vcardFinancier = `BEGIN:VCARD\nVERSION:4.0\nN:;Finanziatore;;\nFN:Finanziatore\nORG:ChatUnity\nTITLE:\nitem1.TEL;waid=393803482529:393803482529\nitem1.X-ABLabel:Finanziatore\nEND:VCARD`;
-    let vcardSocialMediaManager = `BEGIN:VCARD\nVERSION:4.0\nN:;socialmediamanager;;\nFN:socialmediamanager\nORG:ChatUnity\nTITLE:\nitem1.TEL;waid=639076516872:639076516872\nitem1.X-ABLabel:socialmediamanager\nEND:VCARD`;
-    let vcardDeveloper1 = `BEGIN:VCARD\nVERSION:4.0\nN:;Developer;;\nFN:Developer\nORG:ChatUnity\nTITLE:\nitem1.TEL;waid=393935731102:393935731102\nitem1.X-ABLabel:Developer\nEND:VCARD`;
-    let vcardDeveloper2 = `BEGIN:VCARD\nVERSION:4.0\nN:;Developer;;\nFN:Developer\nORG:ChatUnity\nTITLE:\nitem1.TEL;waid=447365589428:447365589428\nitem1.X-ABLabel:Developer\nEND:VCARD`;
-    let vcardDeveloper3 = `BEGIN:VCARD\nVERSION:4.0\nN:;Developer;;\nFN:Developer\nORG:ChatUnity\nTITLE:\nitem1.TEL;waid=393291958600:393291958600\nitem1.X-ABLabel:Developer\nEND:VCARD`;
-    let vcardDeveloper4 = `BEGIN:VCARD\nVERSION:4.0\nN:;Developer;;\nFN:Developer\nORG:ChatUnity\nTITLE:\nitem1.TEL;waid=393518419909:393518419909\nitem1.X-ABLabel:Developer\nEND:VCARD`;
-    let vcardDeveloper5 = `BEGIN:VCARD\nVERSION:4.0\nN:;Developer;;\nFN:Developer\nORG:ChatUnity\nTITLE:\nitem1.TEL;waid=639649477630:639649477630\nitem1.X-ABLabel:Developer\nEND:VCARD`;
+    // Funzione per generare vCard correttamente formattate
+    const createVCard = (name, number, label) => {
+        return `BEGIN:VCARD
+VERSION:3.0
+FN:${name}
+ORG:ChatUnity;
+TEL;type=CELL;type=VOICE;waid=${number}:${number}
+X-ABLabel:${label}
+END:VCARD`.replace(/\n/g, '\r\n'); // Importante per la compatibilità
+    };
 
     await conn.sendMessage(m.chat, { 
         contacts: { 
             displayName: 'ChatUnity Staff', 
             contacts: [
-                { vcard: vcardCreator }, 
-                { vcard: vcardFinancier },
-                { vcard: vcardSocialMediaManager },
-                { vcard: vcardDeveloper1 },
-                { vcard: vcardDeveloper2 },
-                { vcard: vcardDeveloper3 },
-                { vcard: vcardDeveloper4 },
-                { vcard: vcardDeveloper5 }
+                // Contatti originali
+                { vcard: createVCard('Creatore', '393515533859', 'Creatore') },
+                { vcard: createVCard('Finanziatore', '393803482529', 'Finanziatore') },
+                { vcard: createVCard('Social Media Manager', '639076516872', 'Social Media') },
+                
+                // Nuovi developer
+                { vcard: createVCard('Developer 1', '393935731102', 'Developer') },
+                { vcard: createVCard('Developer 2', '447365589428', 'Developer') },
+                { vcard: createVCard('Developer 3', '393291958600', 'Developer') },
+                { vcard: createVCard('Developer 4', '393518419909', 'Developer') },
+                { vcard: createVCard('Developer 5', '639649477630', 'Developer') }
             ] 
         } 
     }, { quoted: m });
 };
+
+handler.help = ['staff'];
+handler.tags = ['info'];
+handler.command = ['staff'];
+
+export default handler;
 
 handler.help = ['staff'];
 handler.tags = ['info'];
