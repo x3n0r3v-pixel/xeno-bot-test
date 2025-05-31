c16a53c247a6a24e01831fd5494b187ca1c86ff4
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-06-01 01:48:47 +0200

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -18,18 +18,24 @@ END:VCARD`.replace(/\n/g, '\r\n'); // Importante per la compatibilità Android
                 { vcard: createVCard('Creatore', '393515533859', 'Creatore') },
                 { vcard: createVCard('Finanziatore', '393803482529', 'Finanziatore') },
                 { vcard: createVCard('Social Media Manager', '639076516872', 'Social Media') },
-                 { vcard: createVCard('Promoter', '393534243103', 'Promoter') }
+                { vcard: createVCard('Promoter', '393534243103', 'Promoter') },
+                
                 // Developer
                 { vcard: createVCard('Developer 1', '393935731102', 'Developer') },
                 { vcard: createVCard('Developer 2', '447365589428', 'Developer') },
                 { vcard: createVCard('Developer 3', '393291958600', 'Developer') },
                 { vcard: createVCard('Developer 4', '393518419909', 'Developer') },
-                { vcard: createVCard('Developer 5', '639649477630', 'Developer') },
-             // Nuovo contatto aggiunto
+                { vcard: createVCard('Developer 5', '639649477630', 'Developer') }
             ] 
         } 
     }, { quoted: m });
 };
+
+handler.help = ['staff'];
+handler.tags = ['info'];
+handler.command = ['staff'];
+
+export default handler;
 
 handler.help = ['staff'];
 handler.tags = ['info'];
