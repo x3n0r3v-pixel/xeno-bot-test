8e7e4ef66f4c644333cac0ea141a9f77ef3bca48
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-06-01 01:51:50 +0200

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -26,7 +26,7 @@ END:VCARD`.replace(/\n/g, '\r\n');
                 { vcard: createVCard('Developer 3', '393291958600', 'Developer') },
                 { vcard: createVCard('Developer 4', '393518419909', 'Developer') },
                 { vcard: createVCard('Developer 5', '639649477630', 'Developer') },
-                { vcard: createVCard('Developer 6', '393534243103', 'Developer') } // Developer aggiuntivo
+            
             ]
         }
     }, { quoted: m });
