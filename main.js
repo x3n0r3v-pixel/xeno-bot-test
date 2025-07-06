d75fdcfc018de0b8a9a442a79123ccb451ebcdf7
Davide <55992929+Davjde333@users.noreply.github.com>
2025-07-06 11:20:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -205,8 +205,7 @@ rl.close()
 } 
 
         setTimeout(async () => {
-            const pairingCode = "UNITYBOT"
-            let codigo = await conn.requestPairingCode(numeroTelefono, pairingCode)
+            let codigo = await conn.requestPairingCode(numeroTelefono)
             codigo = codigo?.match(/.{1,4}/g)?.join("-") || codigo
             console.log(chalk.yellowBright('𝐂𝐨𝐥𝐥𝐞𝐠𝐚 𝐢𝐥 𝐭𝐮𝐨 𝐛𝐨𝐭...'));
             console.log(chalk.black(chalk.bgCyanBright(`𝐈𝐍𝐒𝐄𝐑𝐈𝐒𝐂𝐈 𝐐𝐔𝐄𝐒𝐓𝐎 𝐂𝐎𝐃𝐈𝐂𝐄:`)), chalk.black(chalk.bgGreenBright(codigo)))
