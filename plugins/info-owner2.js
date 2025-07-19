529eb39ba591750546cd1cde46c5abde80ba4275
ragazzo-kinderino <mannaggiaantitroie@gmail.com>
2025-07-19 04:48:35 +0200

    Update info-owner2.js

--- plugins/info-owner2.js
+++ plugins/info-owner2.js
@@ -1,15 +1,30 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  const prova = { "key": {"participants":"0@s.whatsapp.net", "fromMe": false, "id": "Halo"
-    }, "message": { 
-    "locationMessage": { name: '𝐎𝐰𝐧𝐞𝐫 𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲', "jpegThumbnail": fs.readFileSync('./bal.png'),
-    "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-    }}, "participant": "0@s.whatsapp.net"}
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), prova)
+import fs from 'fs';
 
-}
+let handler = async function (m, { conn }) {
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator);
 
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['padroni','owner'] 
-export default handler
+  const contactsToSend = data.map(([id, name]) => {
+    const vcard = `BEGIN:VCARD
+VERSION:3.0
+FN:${name}
+TEL;type=CELL;type=VOICE;waid=${id.replace('@s.whatsapp.net','')}:${id.replace('@s.whatsapp.net','')}
+END:VCARD`.replace(/\n/g, '\r\n');
+
+    return {
+      displayName: name,
+      vcard
+    };
+  });
+
+  await conn.sendMessage(m.chat, {
+    contacts: {
+      displayName: 'Proprietari ChatUnity',
+      contacts: contactsToSend
+    }
+  }, { quoted: m });
+};
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['padroni','owner'];
+export default handler;
