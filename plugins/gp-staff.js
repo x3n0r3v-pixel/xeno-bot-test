d6b943f4f74d55fd3492f9d378fb2b08115d06b0
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-07-22 12:51:56 +0200

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -16,18 +16,17 @@ END:VCARD`.replace(/\n/g, '\r\n');
             contacts: [
                 { vcard: createVCard('Creatore', '393515533859', 'Founder') },
                 { vcard: createVCard('Finanziatore', '393803482529', 'Financer') },
-                { vcard: createVCard('Promoter', '393534243103', 'Promoter') },
 
                 // Team Sviluppo
                 { vcard: createVCard('Developer 1', '393935731102', 'Developer') },
                 { vcard: createVCard('Developer 2', '447365589428', 'Developer') },
                 { vcard: createVCard('Developer 3', '590590596699', 'Developer') },
                 { vcard: createVCard('Developer 4', '393518419909', 'Developer') },
-                { vcard: createVCard('Developer 5', '‪639121920206‬', 'Developer') },
+                { vcard:createVCard('Developer 5', '‪‪639107484127‬‬', 'Developer') },
                 { vcard: createVCard('Developer 6', '393509368693', 'Developer') },
-                { vcard: createVCard('Developer 7', '393201448716', 'Developer') },
+                { vcard: createVCard('Developer 7', '66621409462', 'Developer') },
                 { vcard: createVCard('Developer 8', '393476686131', 'Developer') }
-            ]
+                { vcard: createVCard('Developer 8', '393669729127', 'Developer') }
         }
     }, { quoted: m });
 };
