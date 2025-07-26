87f9eabedfc76b4b31cbd47d34878c7e89383770
Davide <55992929+Davjde333@users.noreply.github.com>
2025-07-26 21:46:45 +0200

    Update admin-deletesession.js

--- plugins/admin-deletesession.js
+++ plugins/admin-deletesession.js
@@ -1,12 +1,11 @@
-// Plugin creato da Gabs & 333 Staff
 import { existsSync, promises as fsPromises } from 'fs';
 import path from 'path';
 
-const handler = async (message, { conn, usedPrefix }) => {
+const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
     return conn.sendMessage(message.chat, {
       text: "*🚨 𝐔𝐭𝐢𝐥𝐢𝐳𝐳𝐢 𝐪𝐮𝐞𝐬𝐭𝐨 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐝𝐢𝐫𝐞𝐭𝐭𝐚𝐦𝐞𝐧𝐭𝐞 𝐧𝐞𝐥 𝐧𝐮𝐦𝐞𝐫𝐨 𝐝𝐞𝐥 𝐛𝐨𝐭.*"
-    }, { quoted: message });
+    }, { quoted: m });
   }
 
   try {
@@ -15,7 +14,7 @@ const handler = async (message, { conn, usedPrefix }) => {
     if (!existsSync(sessionFolder)) {
       return await conn.sendMessage(message.chat, {
         text: "*❌ 𝐋𝐚 𝐜𝐚𝐫𝐭𝐞𝐥𝐥𝐚 𝐝𝐞𝐥𝐥𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢 𝐞̀ 𝐯𝐮𝐨𝐭𝐚 o 𝐧𝐨𝐧 𝐞𝐬𝐢𝐬𝐭𝐞.*"
-      }, { quoted: message });
+      }, { quoted: m });
     }
 
     const sessionFiles = await fsPromises.readdir(sessionFolder);
@@ -27,39 +26,16 @@ const handler = async (message, { conn, usedPrefix }) => {
         deletedCount++;
       }
     }
-
-    const responseText = deletedCount === 0
-      ? "❗ 𝐋𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢 𝐬𝐨𝐧𝐨 𝐯𝐮𝐨𝐭𝐞, 𝐫𝐢𝐩𝐫𝐨𝐯𝐚 𝐭𝐫𝐚 𝐩𝐨𝐜𝐨 𝐧𝐞 𝐡𝐨 𝐛𝐢𝐬𝐨𝐠𝐧𝐨 ‼️"
-      : `🔥 𝐒𝐨𝐧𝐨 𝐬𝐭𝐚𝐭𝐢 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐭𝐢 ${deletedCount} 𝐚𝐫𝐜𝐡𝐢𝐯𝐢 𝐝𝐞𝐥𝐥𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢! 𝐆𝐫𝐚𝐳𝐢𝐞 𝐩𝐞𝐫 𝐚𝐯𝐞𝐫𝐦𝐢 𝐬𝐯𝐮𝐨𝐭𝐚𝐭𝐨😼`;
-
-    await conn.sendMessage(message.chat, { text: responseText }, { quoted: message });
-
+ 
+    await conn.sendMessage(message.chat, { text: `${deletedCount === 0 ? '❗ 𝐋𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢 𝐬𝐨𝐧𝐨 𝐯𝐮𝐨𝐭𝐞, 𝐫𝐢𝐩𝐫𝐨𝐯𝐚 𝐭𝐫𝐚 𝐩𝐨𝐜𝐨 𝐧𝐞 𝐡𝐨 𝐛𝐢𝐬𝐨𝐠𝐧𝐨 ‼️' : `🔥 𝐒𝐨𝐧𝐨 𝐬𝐭𝐚𝐭𝐢 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐭𝐢 ${deletedCount} 𝐚𝐫𝐜𝐡𝐢𝐯𝐢 𝐝𝐞𝐥𝐥𝐞 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢! 𝐆𝐫𝐚𝐳𝐢𝐞 𝐩𝐞𝐫 𝐚𝐯𝐞𝐫𝐦𝐢 𝐬𝐯𝐮𝐨𝐭𝐚𝐭𝐨😼` }, { quoted: m });
   } catch (error) {
-    console.error('⚠️ Errore:', error);
-    await conn.sendMessage(message.chat, { text: "❌ 𝐄𝐫𝐫𝐨𝐫𝐞 𝐝𝐢 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐳𝐢𝐨𝐧𝐞!" }, { quoted: message });
+    await conn.sendMessage(message.chat, { text: "❌ 𝐄𝐫𝐫𝐨𝐫𝐞 𝐝𝐢 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐳𝐢𝐨𝐧𝐞!" }, { quoted: m });
   }
-
-  const botName = global.db.data.nomedelbot || "ChatUnity x 333";
-  const quotedMessage = {
-    key: {
-      participants: "0@s.whatsapp.net",
-      fromMe: false,
-      id: 'Halo'
-    },
-    message: {
-      locationMessage: {
-        name: botName,
-        jpegThumbnail: Buffer.from(await (await fetch("https://qu.ax/cSqEs.jpg")).arrayBuffer()),
-        vcard: "BEGIN:VCARD\nVERSION:3.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=19709001746:+1 (970) 900-1746\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD"
-      }
-    },
-    participant: '0@s.whatsapp.net'
-  };
 };
 
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ["owner"];
-handler.command = /^(deletession|ds|clearallsession)$/i;
+handler.command = /^(deletession|ds)$/i;
 handler.admin = true;
 
 export default handler;
