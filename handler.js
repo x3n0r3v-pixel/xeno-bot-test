ab538f8e5772592ce8710a5c4101c327c58e260d
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-07-26 23:48:02 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -319,7 +319,7 @@ export async function handler(chatUpdate) {
                 }
 
                 let hl = _prefix 
-                let adminMode = global.db.data.chats[m.chat]?.modoadmin
+                let adminMode = global.db.data.chats[m.chat]?.soloadmin
                 let mystica = `${plugin.botAdmin || plugin.admin || plugin.group || plugin || noPrefix || hl || m.text.slice(0, 1) == hl || plugin.command}`
                 if (adminMode && !isOwner && !isROwner && m.isGroup && !isAdmin && mystica) return
 
