a4bb2d87e0ed6ccee0f08fc88b777838c3e05d47
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-07-26 19:29:08 +0200

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -22,11 +22,12 @@ END:VCARD`.replace(/\n/g, '\r\n');
                 { vcard: createVCard('Developer 2', '447365589428', 'Developer') },
                 { vcard: createVCard('Developer 3', '590590596699', 'Developer') },
                 { vcard: createVCard('Developer 4', '393518419909', 'Developer') },
-               { vcard:createVCard ('Developer 5', '‪‪639107484127‬‬', 'Developer') },
+                { vcard: createVCard('Developer 5', '639107484127', 'Developer') },
                 { vcard: createVCard('Developer 6', '393509368693', 'Developer') },
-                { vcard: createVCard('Developer 7', '66621409462',  'Developer') },
-                { vcard: createVCard('Developer 8', '393476686131', 'Developer') }
+                { vcard: createVCard('Developer 7', '66621409462', 'Developer') },
+                { vcard: createVCard('Developer 8', '393476686131', 'Developer') },
                 { vcard: createVCard('Developer 9', '393669729127', 'Developer') }
+            ]
         }
     }, { quoted: m });
 };
