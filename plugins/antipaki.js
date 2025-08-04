b630093a74ce0c5d2345a90d5a0bf80268863bed
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-08-04 16:08:13 +0200

    Update antipaki.js

--- plugins/antipaki.js
+++ plugins/antipaki.js
@@ -6,7 +6,7 @@ handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwne
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[conn.user.jid] || {}
     
-    if (isBotAdmin && chat.antiArab && !isAdmin && !isOwner && !isROwner && bot.restrict) {
+    if (isBotAdmin && chat.antipaki && !isAdmin && !isOwner && !isROwner && bot.restrict) {
         if (m.sender.startsWith('92')) {
             let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
             if (responseb[0].status === "404") return
