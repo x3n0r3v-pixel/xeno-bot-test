f64271dcc3c8bf0bb246e3db616012cfef3aa1cc
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-08-15 17:17:24 +0200

    Update info-creatore.js

--- plugins/info-creatore.js
+++ plugins/info-creatore.js
@@ -21,5 +21,5 @@ END:VCARD`.replace(/\n/g, '\r\n');
 
 handler.help = ['creatore'];
 handler.tags = ['info'];
-handler.command = ['creatore', 'proprietario', ]; // ✅ Qui la correzione
+handler.command = ['creatore', ]; // ✅ Qui la correzione
 export default handler;
