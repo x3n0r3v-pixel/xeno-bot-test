5f4ebb4b6464944b4804afcd184aeb12d415a30b
x3n0r3v-pixel <x3n0.r3v@gmail.com>
2025-09-22 21:30:22 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -4,12 +4,15 @@ module.exports = {
   async execute(client, message, args) {
     const text = `
 📜 *Comandi disponibili:*
-- .tag → menziona tutti senza mostrare le @ (hidetag)
+- .tag → menziona tutti i membri del gruppo in un messaggio
 - .tagall → menziona tutti mostrando le @
 - .comunicazione → fissa il messaggio e menziona tutti (hidetag)
 - .pin → fissa il messaggio a cui rispondi
 - .kick → espelle il membro a cui rispondi
 - .menu → mostra questo menu
+
+
+xeno - bot (xeno domina)
     `;
     await message.reply(text);
   }
